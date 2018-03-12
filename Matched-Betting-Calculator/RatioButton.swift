ea814db7dfc6559a56d14790735e62416476ac65
fedemengo <fede.mengo@hotmail.it>
2018-03-12 09:24:58 +0100

    add ratio button

---
+++ Matched-Betting-Calculator/RatioButton.swift
@@ -0,0 +1,58 @@
+//
+//  RatioButton.swift
+//  Matched-Betting-Calculator
+//
+//  Created by Federico Mengozzi on 12/03/2018.
+//  Copyright © 2018 Federico Mengozzi. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class RatioButton : UIButton {
+    
+    var buttons:Array<RatioButton>?
+    var buttonIsSelected: Bool?
+    
+    func unselectButtons(){
+        
+        if buttons != nil {
+            
+            self.isSelected = true
+            
+            for aButton:RatioButton in buttons! {
+                aButton.isSelected = false
+            }
+            
+        } else {
+            toggleButton()
+        }
+    }
+    
+    override var isSelected: Bool {
+        willSet (isSelected) {
+            if(isSelected){
+                self.backgroundColor = UIColor.green
+                self.setTitleColor(UIColor.black, for: UIControlState.normal)
+                print(self.titleLabel!.text! + " is selected")
+            }
+            else {
+                self.backgroundColor = UIColor.lightGray
+                self.setTitleColor(UIColor.gray, for: UIControlState.normal)
+            }
+        }
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.unselectButtons()
+        super.touchesBegan(touches, with: event)
+    }
+    
+    func toggleButton(){
+        if self.isSelected {
+            self.isSelected = false
+        } else {
+            self.isSelected = true
+        }
+    }
+}
