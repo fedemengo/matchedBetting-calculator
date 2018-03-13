a92de81ee2a0b2fad15e04b15b2010645517f8d9
fedemengo <fede.mengo@hotmail.it>
2018-03-13 20:41:19 +0100

    add enum for bet type

---
+++ Matched-Betting-Calculator/Bet.swift
@@ -0,0 +1,14 @@
+//
+//  Bet.swift
+//  Matched-Betting-Calculator
+//
+//  Created by Federico Mengozzi on 13/03/2018.
+//  Copyright © 2018 Federico Mengozzi. All rights reserved.
+//
+
+import Foundation
+
+enum BET {
+    case QUAL
+    case FREE
+}
---
+++ Matched-Betting-Calculator/BetCalculator.swift
@@ -0,0 +1,9 @@
+//
+//  BetCalculator.swift
+//  Matched-Betting-Calculator
+//
+//  Created by Federico Mengozzi on 13/03/2018.
+//  Copyright © 2018 Federico Mengozzi. All rights reserved.
+//
+
+import Foundation
