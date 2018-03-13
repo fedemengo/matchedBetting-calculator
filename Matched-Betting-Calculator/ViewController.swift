624ca2b58a1d2161c1c6434ef682573fa500b4bd
fedemengo <fede.mengo@hotmail.it>
2018-03-13 20:45:52 +0100

    general refactor

--- Matched-Betting-Calculator/ViewController.swift
+++ Matched-Betting-Calculator/ViewController.swift
@@ -8,39 +8,7 @@
 
 import UIKit
 
-extension String {
-    var floatValue: Float {
-        let nf = NumberFormatter()
-        nf.decimalSeparator = "."
-        if let result = nf.number(from: self) {
-            return result.floatValue
-        } else {
-            nf.decimalSeparator = ","
-            if let result = nf.number(from: self) {
-                return result.floatValue
-            }
-        }
-        return Float.nan
-    }
-    
-    var doubleValue:Double {
-        let nf = NumberFormatter()
-        nf.decimalSeparator = "."
-        if let result = nf.number(from: self) {
-            return result.doubleValue
-        } else {
-            nf.decimalSeparator = ","
-            if let result = nf.number(from: self) {
-                return result.doubleValue
-            }
-        }
-        return Double.nan
-    }
-}
-
 class ViewController: UIViewController {
-    
-    let ROUND: Double = 100.0
     
     @IBOutlet weak var backStake: UITextField!
     @IBOutlet weak var backOdds: UITextField!
@@ -56,97 +24,63 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var author: UILabel!
     
-    @IBOutlet var qualifyingBetButton: RatioButton!
+    @IBOutlet var qualBetButton: RatioButton!
     @IBOutlet var freeBetButton: RatioButton!
+    
+    let calculator = BetCalculator()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         author.text = "\u{f09b} @fedemengo"
         
-        qualifyingBetButton.layer.cornerRadius = qualifyingBetButton.frame.width/2
-        freeBetButton.layer.cornerRadius = freeBetButton.frame.width/2
-        
-        qualifyingBetButton.isSelected = true
-        freeBetButton.isSelected = false
-        
-        qualifyingBetButton?.buttons = [freeBetButton!]
-        freeBetButton?.buttons = [qualifyingBetButton!]
-        
-        qualifyingBetButton.addTarget(self, action:#selector(calculate), for: .touchUpInside)
-        freeBetButton.addTarget(self, action:#selector(calculate), for: .touchUpInside)
-
+        initButton(btn: qualBetButton, betType: BET.QUAL, selected: true, otherBtn: freeBetButton)
+        initButton(btn: freeBetButton, betType: BET.FREE, selected: false, otherBtn: qualBetButton)
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
-    override func resignFirstResponder() -> Bool {
+    func closeKey() {
         backStake.resignFirstResponder()
         backOdds.resignFirstResponder()
         backFee.resignFirstResponder()
         layOdds.resignFirstResponder()
         layFee.resignFirstResponder()
-        return true;
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        print("touch intercepted")
-        resignFirstResponder()
+        closeKey()
         calculate()
     }
     
-    func parseDouble(string: String) -> Double {
-        if string == "" {
-            return 0.0
-        }
-        else {
-            return string.doubleValue
-        }
-    }
-    
-    func validate(bs: Double, bo: Double, bc: Double, lo: Double, lc: Double) -> Bool {
-        if bs == 0.0 || bs.isNaN || bo == 0.0 || bo.isNaN || bc.isNaN || lo == 0.0 || lo.isNaN || lc.isNaN {
-            return false;
-        }
-        return true;
-    }
-    
-    func roundDouble(x: Double) -> Double {
-        return Double(round(ROUND*x)/ROUND)
+    func initButton(btn: RatioButton, betType: BET, selected: Bool, otherBtn: RatioButton) -> Void {
+        btn.layer.cornerRadius = btn.frame.width/2
+        btn.isSelected = selected
+        btn.bet = betType
+        btn.buttons = [otherBtn]
+        btn.addTarget(self, action:#selector(calculate), for: .touchUpInside)
     }
     
     func calculate() -> Void {
-        let isQualifyingBet = qualifyingBetButton.isSelected
-        let backStakeValue = parseDouble(string: backStake.text!)
-        let backOddsValue = parseDouble(string: backOdds.text!)
-        let backCommisionValue = parseDouble(string: backFee.text!)
-        let layOddsValue = parseDouble(string: layOdds.text!)
-        let layCommissionValue = parseDouble(string: layFee.text!)
+        let betType = (qualBetButton.isSelected ? qualBetButton.bet : freeBetButton.bet)
+        let backStakeValue = Utility.parseDouble(string: backStake.text!)
+        let backOddsValue = Utility.parseDouble(string: backOdds.text!)
+        let backCommisionValue = Utility.parseDouble(string: backFee.text!)
+        let layOddsValue = Utility.parseDouble(string: layOdds.text!)
+        let layCommissionValue = Utility.parseDouble(string: layFee.text!)
         
-        if validate(bs: backStakeValue, bo: backOddsValue, bc: backCommisionValue, lo: layOddsValue, lc: layCommissionValue) {
-            var layStakeValue: Double
-            var backLose: Double
+        if BetManager.isValid(backStack: backStakeValue, backOdds: backOddsValue, backComm: backCommisionValue, layOdds: layOddsValue, layComm: layCommissionValue) {
             
-            if(isQualifyingBet){
-                layStakeValue = roundDouble(x: (backStakeValue*(backOddsValue-backCommisionValue))/(layOddsValue-layCommissionValue))
-                backLose = roundDouble(x: -backStakeValue)
-            }
-            else {
-                layStakeValue = roundDouble(x:(backStakeValue*(backOddsValue-1-backCommisionValue))/(layOddsValue-layCommissionValue))
-                backLose = -0.0
-            }
+            calculator.initBet(betType: betType!, backStake: backStakeValue, backOdds: backOddsValue, backCom: backCommisionValue, layOdds: layOddsValue, layCom: layCommissionValue)
             
-            let backWins = roundDouble(x: backStakeValue*(backOddsValue-1-backCommisionValue))
-            let layLose = roundDouble(x: -layStakeValue*(1-layOddsValue))
+            layStake.text = "\(calculator.getLayStake())"
+            backWin.text = calculator.caseBackWins()
+            layWin.text = calculator.caseLayWins()
             
-            let layWins = roundDouble(x: layStakeValue*(1-layCommissionValue))
-            
-            layStake.text = "\(layStakeValue)"
-            backWin.text = "\(backWins) - \(layLose) = \(roundDouble(x: backWins-layLose))"
-            layWin.text = "\(backLose) + \(layWins) = \(roundDouble(x: backLose+layWins))"
-            profit.text = "\(roundDouble(x: min(backWins-layLose, backLose+layWins)))"
+            profit.font = UIFont.boldSystemFont(ofSize: 17)
+            profit.textColor = calculator.getProfit() > 0 ? UIColor.green : UIColor.red
+            profit.text = "\(calculator.getProfit())"
         }
     }
     
