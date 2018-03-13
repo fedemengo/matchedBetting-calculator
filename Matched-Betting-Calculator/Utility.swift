2a89c73f036414799bec3ffa2f8749467c096aaa
fedemengo <fede.mengo@hotmail.it>
2018-03-13 20:42:38 +0100

    add utility class for I/O parsing

--- Matched-Betting-Calculator/Utility.swift
+++ Matched-Betting-Calculator/Utility.swift
@@ -7,3 +7,50 @@
 //
 
 import Foundation
+
+extension String {
+    var floatValue: Float {
+        let nf = NumberFormatter()
+        nf.decimalSeparator = "."
+        if let result = nf.number(from: self) {
+            return result.floatValue
+        } else {
+            nf.decimalSeparator = ","
+            if let result = nf.number(from: self) {
+                return result.floatValue
+            }
+        }
+        return Float.nan
+    }
+    
+    var doubleValue:Double {
+        let nf = NumberFormatter()
+        nf.decimalSeparator = "."
+        if let result = nf.number(from: self) {
+            return result.doubleValue
+        } else {
+            nf.decimalSeparator = ","
+            if let result = nf.number(from: self) {
+                return result.doubleValue
+            }
+        }
+        return Double.nan
+    }
+}
+
+class Utility {
+    static let ROUND: Double = 100.0
+    
+    static func parseDouble(string: String) -> Double {
+        if string == "" {
+            return 0.0
+        }
+        else {
+            return string.doubleValue
+        }
+    }
+    
+    static func roundDouble(x: Double) -> Double {
+        return Double(round(ROUND*x)/ROUND)
+    }
+}
